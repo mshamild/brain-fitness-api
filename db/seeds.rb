b67ad6fcd4ed433059dd8e7d35cc57984d5eda0d
@ilnurnasyrov <ilnur.nasyrov@flatstack.com>
2015-08-07 18:54:43 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+[
+  { file: 'js_questions.json', title: 'JavaScript', color: '#0000ff' },
+  { file: 'ruby_questions.json', title: 'Ruby', color: '#ff0000' }
+].each do |attributes|
+  category = Category.find_or_create_by! attributes.slice(:title, :color)
+
+  questions_attributes =
+    JSON.parse \
+    IO.read \
+    Rails.root.join('db', 'seeds', 'data', attributes[:file])
+
+  questions_attributes.each do |attributes|
+    question = Question.find_or_create_by!(text: attributes['question'], category: category)
+
+    attributes['answers'].each do |attributes|
+      AnswerVariant.find_or_create_by! attributes.merge(question: question)
+    end
+  end
+end
