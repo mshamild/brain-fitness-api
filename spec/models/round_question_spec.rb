b0a36a7508685c7fd786c4a69004ad8f0e9355a9
@ilnurnasyrov <ilnur.nasyrov@flatstack.com>
2015-08-12 18:35:26 +0300

    Fix round questions spec

--- spec/models/round_question_spec.rb
+++ spec/models/round_question_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe RoundQuestion, type: :model do
   describe 'associations' do
     it { is_expected.to belong_to(:round_category) }
     it { is_expected.to belong_to(:question) }
-    it { is_expected.to have_many(:answer_variants) }
   end
 
   describe 'validations' do
